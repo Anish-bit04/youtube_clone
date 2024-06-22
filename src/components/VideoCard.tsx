724074035c4f40300968c47051a3c9d480e97fe8
Anish <work.anish.004@gmail.com>
2024-06-22 13:14:42 +0530

    started yt-clone

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const VideoCard = () => {
+  return (
+    <div>
+        <img src="./image.png" alt="Image" />
+        <div className='grid grid-cols-12'>
+             <div className='col`-span-2'>
+                <img src="./image.png" alt="fddfb" /> </div>
+        <div className='col-span-10'>
+            Rust Tutorial For Beginners | Two Rust
+            Projects | Rust Notes Included | Hindi
+            Code Eater
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default VideoCard
